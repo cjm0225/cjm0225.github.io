c2ee912eeb3cf2d881b96add77019a30eb2ccb5b
cjm0225 <45387377+cjm0225@users.noreply.github.com>
2020-10-12 17:09:26 +0800

    创建需要的基础页面

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <Header></Header>
+    <Nuxt />
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// 在这里引入的组件会在所有页面都显示,这个文件相当于vue-cli的app.js,负责显示总体显示
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+
+<style lang="less" >
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+em,
+i {
+  font-style: normal;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-export default {};
 </script>
 
-<style>
+
+<style lang="less" scoped>
 </style>
